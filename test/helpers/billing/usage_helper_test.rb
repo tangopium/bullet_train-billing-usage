5ac181bb58b05b2fe29214da75f6ef2696796180
Jamie Wright <jamie@brilliantfantastic.com>
2023-01-10 10:14:27 -0500

    Switch to minitest spec to make the organization better

--- test/helpers/billing/usage_helper_test.rb
+++ test/helpers/billing/usage_helper_test.rb
@@ -7,54 +7,53 @@ class Billing::UsageHelperTest < ActiveSupport::TestCase
     include Billing::UsageHelper
   end
 
-  setup do
-    @model = DummyClass
+  describe "#broken_hard_limits_message" do
+    let(:controller) { DummyController.new }
+    let(:limits) { [{action: :have, usage: 3, limit: {"count" => 3, "enforcement" => "hard", "upgradable" => true, "product_id" => "basic"}}] }
+    let(:limiter) { Minitest::Mock.new }
+    let(:model) { DummyClass }
 
-    @limits = [{action: :have, usage: 3, limit: {"count" => 3, "enforcement" => "hard", "upgradable" => true, "product_id" => "basic"}}]
-    @limiter = Minitest::Mock.new
-    @limiter.expect :broken_hard_limits_for, @limits, [:create, @model], count: Integer
+    before { limiter.expect :broken_hard_limits_for, limits, [:create, model], count: Integer }
 
-    @dummy = DummyController.new
-  end
+    it "returns a message that you can't anymore of the model if you are trying to add more" do
+      message = controller.broken_hard_limits_message(limiter, model)
 
-  test "#broken_hard_limits_message returns a message that you can't anymore of the model if you are trying to add more" do
-    message = @dummy.broken_hard_limits_message(@limiter, @model)
+      assert_includes message, "You can't add a Membership"
+    end
 
-    assert_includes message, "You can't add a Membership"
-  end
+    it "returns the broken limits alongside the current limits" do
+      message = controller.broken_hard_limits_message(limiter, model)
 
-  test "#broken_hard_limits_message returns the broken limits alongside the current limits" do
-    message = @dummy.broken_hard_limits_message(@limiter, @model)
+      assert_includes message, "because you already have 3 out of 3 Memberships"
+    end
 
-    assert_includes message, "because you already have 3 out of 3 Memberships"
-  end
+    it "returns a message that you already have if you are not trying to add more" do
+      message = controller.broken_hard_limits_message(limiter, model, count: 0)
 
-  test "#broken_hard_limits_message returns a message that you already have if you are not trying to add more" do
-    message = @dummy.broken_hard_limits_message(@limiter, @model, count: 0)
+      assert_includes message, "You've used 3 of 3 Memberships"
+    end
 
-    assert_includes message, "You've used 3 of 3 Memberships"
-  end
+    it "returns the product name that had the broken limits" do
+      message = controller.broken_hard_limits_message(limiter, model)
 
-  test "#broken_hard_limits_message returns the product name that had the broken limits" do
-    message = @dummy.broken_hard_limits_message(@limiter, @model)
+      assert_includes message, "allowed by your Basic account."
+    end
 
-    assert_includes message, "allowed by your Basic account."
-  end
+    it "returns the broken limits for another action" do
+      limits.first[:action] = :create
 
-  test "#broken_hard_limits_message returns the broken limits for another action" do
-    @limits.first[:action] = :create
+      message = controller.broken_hard_limits_message(limiter, model)
 
-    message = @dummy.broken_hard_limits_message(@limiter, @model)
+      assert_includes message, "You can't create a Membership because you've already created 3 out of 3 Memberships"
+    end
 
-    assert_includes message, "You can't create a Membership because you've already created 3 out of 3 Memberships"
-  end
+    it "returns the interval if that is specified in the config" do
+      limits.first[:action] = :create
+      limits.first[:limit].merge!({"duration" => 1, "interval" => "months"})
 
-  test "#broken_hard_limits_message returns the interval if that is specified in the config" do
-    @limits.first[:action] = :create
-    @limits.first[:limit].merge!({"duration" => 1, "interval" => "months"})
+      message = controller.broken_hard_limits_message(limiter, model)
 
-    message = @dummy.broken_hard_limits_message(@limiter, @model)
-
-    assert_includes message, "allowed by your Basic account in the current 1 month period."
+      assert_includes message, "allowed by your Basic account in the current 1 month period."
+    end
   end
 end
