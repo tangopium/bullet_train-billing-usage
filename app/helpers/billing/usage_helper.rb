8045c0ef89ddb6729a07036c9ceab752514b0a97
Jamie Wright <jamie@brilliantfantastic.com>
2023-01-10 11:02:12 -0500

    Added broken_soft_limit_message to the helper with tests

--- app/helpers/billing/usage_helper.rb
+++ app/helpers/billing/usage_helper.rb
@@ -3,9 +3,18 @@ module Billing::UsageHelper
 
   def broken_hard_limits_message(limiter, model, action: :create, count: 1)
     limiter.broken_hard_limits_for(action, model, count: count).each_with_index.reduce([]) do |message_parts, (limit, index)|
-      message_parts << broken_hard_limits_introduction(model, limit, index: index, count: count)
+      message_parts << broken_limits_introduction(model, limit, index: index, count: count)
       message_parts << broken_hard_limits_usage(limit, count: count)
-      message_parts << broken_hard_limits_limit(model, limit)
+      message_parts << broken_limits_limit(model, limit)
+    end.flatten.join(" ")
+  end
+
+  def broken_soft_limits_message(limiter, model, action: :create, count: 1)
+    limiter.broken_soft_limits_for(action, model, count: count).each_with_index.reduce([]) do |message_parts, (limit, index)|
+      message_parts << broken_limits_introduction(model, limit, index: index, count: count)
+      message_parts << broken_soft_limits_usage(limit, count: count)
+      message_parts << broken_limits_limit(model, limit)
+      message_parts
     end.flatten.join(" ")
   end
 
@@ -17,29 +26,27 @@ module Billing::UsageHelper
 
   private
 
-  def broken_hard_limits_introduction(model, limit, index:, count:)
+  def broken_limits_introduction(model, limit, count:, index:)
     action = limit[:action]
 
-    if action == :have && count.zero? # already exhausted product
-      return ["You've"]
-    end
+    return ["You've"] if !display_unavailable_action?(limit, count)
 
     introduction = ["You"]
     introduction << "also" if index > 0
     introduction << "can't"
     introduction << (action == :have ? "add" : action)
     introduction << (count == 1 ? "a" : number_with_delimiter(count))
-    introduction << broken_hard_limits_model_name(model, count: count)
+    introduction << broken_limits_model_name(model, count: count)
   end
 
-  def broken_hard_limits_limit(model, limit)
+  def broken_limits_limit(model, limit)
     limit_count = limit.dig(:limit, "count")
     duration = limit.dig(:limit, "duration") || 1
     interval = limit.dig(:limit, "interval")
     product_id = limit.dig(:limit, "product_id")
 
     limit = [number_with_delimiter(limit_count)]
-    limit << broken_hard_limits_model_name(model, count: limit_count)
+    limit << broken_limits_model_name(model, count: limit_count)
     limit << "allowed by your"
     limit << I18n.t("billing/products.#{product_id}.name")
     if interval.nil?
@@ -51,7 +58,7 @@ module Billing::UsageHelper
     limit
   end
 
-  def broken_hard_limits_model_name(model, count:)
+  def broken_limits_model_name(model, count:)
     I18n.t("#{model.name.underscore.pluralize}.label").singularize.pluralize(count)
   end
 
@@ -68,5 +75,22 @@ module Billing::UsageHelper
 
     usage << number_with_delimiter(limit[:usage])
     usage << (action == :have && count.zero? ? "of" : "out of")
+  end
+
+  def broken_soft_limits_usage(limit, count:)
+    action = limit[:action]
+
+    usage = []
+
+    usage << (action == :have ? "used" : action.verb.conjugate(tense: :past))
+
+    usage << number_with_delimiter(limit[:usage])
+    usage << (action == :have && count.zero? ? "of" : "out of")
+  end
+
+  def display_unavailable_action?(limit, count)
+    enforcement = limit.dig(:limit, "enforcement")
+
+    !(limit[:action] == :have && count.zero?) && enforcement == "hard"
   end
 end
--- test/helpers/billing/usage_helper_test.rb
+++ test/helpers/billing/usage_helper_test.rb
@@ -15,7 +15,7 @@ class Billing::UsageHelperTest < ActiveSupport::TestCase
 
     before { limiter.expect :broken_hard_limits_for, limits, [:create, model], count: Integer }
 
-    it "returns a message that you can't anymore of the model if you are trying to add more" do
+    it "returns a message that you can't create anymore of the model if you are trying to add more" do
       message = controller.broken_hard_limits_message(limiter, model)
 
       assert_includes message, "You can't add a Membership"
@@ -52,6 +52,44 @@ class Billing::UsageHelperTest < ActiveSupport::TestCase
       limits.first[:limit].merge!({"duration" => 1, "interval" => "months"})
 
       message = controller.broken_hard_limits_message(limiter, model)
+
+      assert_includes message, "allowed by your Basic account in the current 1 month period."
+    end
+  end
+
+  describe "#broken_soft_limits_message" do
+    let(:controller) { DummyController.new }
+    let(:limits) { [{action: :have, usage: 3, limit: {"count" => 3, "enforcement" => "soft", "product_id" => "basic"}}] }
+    let(:limiter) { Minitest::Mock.new }
+    let(:model) { DummyClass }
+
+    before { limiter.expect :broken_soft_limits_for, limits, [:create, model], count: Integer }
+
+    it "returns a message of what you already have" do
+      message = controller.broken_soft_limits_message(limiter, model, count: 1)
+
+      assert_includes message, "You've used 3 out of 3 Memberships"
+    end
+
+    it "returns the product name that had the broken limits" do
+      message = controller.broken_soft_limits_message(limiter, model)
+
+      assert_includes message, "allowed by your Basic account."
+    end
+
+    it "returns the broken limits for another action" do
+      limits.first[:action] = :create
+
+      message = controller.broken_soft_limits_message(limiter, model, count: 1)
+
+      assert_includes message, "You've created 3 out of 3 Memberships"
+    end
+
+    it "returns the interval if that is specified in the config" do
+      limits.first[:action] = :create
+      limits.first[:limit].merge!({"duration" => 1, "interval" => "months"})
+
+      message = controller.broken_soft_limits_message(limiter, model)
 
       assert_includes message, "allowed by your Basic account in the current 1 month period."
     end
