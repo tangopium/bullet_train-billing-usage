cadcffcabcb4c253314a7271f3c543164d57e317
Jamie Wright <jamie@brilliantfantastic.com>
2023-01-18 19:54:54 -0500

    Refactor to keep the encapsulation more pure

--- app/models/concerns/billing/usage/has_trackers.rb
+++ app/models/concerns/billing/usage/has_trackers.rb
@@ -2,17 +2,16 @@ module Billing::Usage::HasTrackers
   extend ActiveSupport::Concern
 
   included do
-    has_many :billing_usage_trackers, class_name: "Billing::Usage::Tracker", dependent: :destroy
-  end
+    has_many :billing_usage_trackers, class_name: "Billing::Usage::Tracker", dependent: :destroy do
+      def current
+        Billing::Usage::Tracker.cycles.map do |cycle|
+          duration, interval = cycle
 
-  # TODO Would be great to figure out how to do this as a scope on `billing_usage_trackers`.
-  def current_billing_usage_trackers
-    Billing::Usage::Tracker.cycles.map do |cycle|
-      duration, interval = cycle
-
-      # This will grab the most recent tracker for this usage cycle.
-      # If it doesn't exist, it will be created. This can happen if developers introduce new usage cycles to track by.
-      billing_usage_trackers.order(created_at: :desc).find_or_create_by(duration: duration, interval: interval)
+          # This will grab the most recent tracker for this usage cycle.
+          # If it doesn't exist, it will be created. This can happen if developers introduce new usage cycles to track by.
+          order(created_at: :desc).find_or_create_by(duration: duration, interval: interval)
+        end
+      end
     end
   end
 end
--- lib/tasks/billing.rake
+++ lib/tasks/billing.rake
@@ -2,7 +2,7 @@ namespace :billing do
   desc "Create new usage trackers as needed"
   task cycle_usage_trackers: :environment do
     Team.find_each do |team|
-      team.current_billing_usage_trackers.each do |tracker|
+      team.billing_usage_trackers.current.each do |tracker|
         tracker.cycle_as_needed
       end
     end
