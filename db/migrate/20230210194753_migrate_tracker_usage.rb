ef519c079c48bd41b14ef7e23cbe374e126febb6
Jamie Wright <jamie@brilliantfantastic.com>
2023-02-10 18:00:57 -0500

    Use a migration to migrate the usage data since that is the pattern in BT

---
+++ db/migrate/20230210194753_migrate_tracker_usage.rb
@@ -0,0 +1,46 @@
+class MigrateTrackerUsage < ActiveRecord::Migration[7.0]
+  def up
+    trackers do |tracker|
+      (tracker.usage || {}).map do |model, counts|
+        counts.map do |action, count|
+          tracker.counts.upsert({action: action,
+                                 name: model.to_s,
+                                 tracker_id: tracker,
+                                 count: count},
+                                unique_by: [:action, :name, :tracker_id])
+        end
+      end
+    end
+  end
+
+  def down
+    trackers do |tracker|
+      tracker.counts.order(created_at: :desc).each do |count|
+        count.tracker.usage[count.name] ||= {}
+        count.tracker.usage[count.name][count.action] = count.count
+        count.tracker.save
+      end
+      tracker.counts.destroy_all
+    end
+  end
+
+  private
+
+  def trackers
+    Billing::Usage.trackers.each do |tracker|
+      tracker.find_each do |has_trackers|
+        Billing::Usage::Tracker.cycles(has_trackers).map do |cycle|
+          duration, interval = cycle
+
+          has_trackers
+            .billing_usage_trackers
+            .order(created_at: :desc)
+            .includes(:counts)
+            .find_by(duration: duration, interval: interval)
+        end.each do |tracker|
+          yield tracker
+        end
+      end
+    end
+  end
+end
